bfbf0ae2e536950a0874d18e74bf90d28387b424
zhyq0826 <zhyq0826@gmail.com>
2014-12-22 15:36:26 +0800

    add class-attr

---
+++ test_class_attr.py
@@ -0,0 +1,29 @@
+class Base(object):
+
+    _instance = {}
+
+    @staticmethod
+    def instance(key):
+        if not Base._instance.get(key):
+            Base._instance[key] = 11
+
+        return Base._instance[key]
+
+
+
+class A(Base):
+    pass
+
+
+class B(Base):
+    pass
+
+
+if __name__ == '__main__':
+    a = A()
+    b = B()
+    b.instance('hello')
+    a.instance('h')
+    print(b._instance) 
+    print(a._instance)
+    print a._instance == b._instance 
